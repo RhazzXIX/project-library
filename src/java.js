b5a267b24aa12c966674e53a27f3038c9515aa64
RhazzXIX <jm21tonio@gmail.com>
2023-04-10 12:53:50 +0800

    Refactor to support cloud firestore

--- src/java.js
+++ src/java.js
@@ -1,6 +1,16 @@
-import './styles/style.css'
-import { initializeApp } from 'firebase/app'
-import {collection, addDoc, getDocs, getFirestore} from 'firebase/firestore'
+import "./styles/style.css";
+import "./index.html";
+import { initializeApp } from "firebase/app";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+  setDoc,
+  updateDoc,
+  getFirestore,
+} from "firebase/firestore";
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyAwzlgosGqSGsrx4CzJK4liK09JnJ1Dy9w",
@@ -9,22 +19,24 @@ const firebaseApp = initializeApp({
   storageBucket: "project-library-14926.appspot.com",
   messagingSenderId: "459523889275",
   appId: "1:459523889275:web:2ad594c8e377bdd0c82ade",
-  measurementId: "G-488283056F"
-})
+  measurementId: "G-488283056F",
+});
 
-const database = getFirestore(firebaseApp)
-
+const database = getFirestore(firebaseApp);
 
 const shelve = document.querySelector("main#shelve");
 const body = document.querySelector("body");
 
 const addBooksBtn = document.querySelector("button#addBook");
+const closeFormBtn = document.querySelector("button#close-form");
+const form = document.querySelector("section.form-container");
+const notice = document.querySelector("section#notice");
+
 addBooksBtn.addEventListener("click", (e) => {
   body.appendChild(form);
   e.stopPropagation();
 });
 
-const form = document.querySelector("section.form-container");
 form.addEventListener(
   "mousedown",
   (e) => {
@@ -35,10 +47,14 @@ form.addEventListener(
   true
 );
 
-const closeFormBtn = document.querySelector("button#close-form");
 closeFormBtn.addEventListener("click", (e) => {
   e.stopPropagation();
   body.removeChild(form);
+});
+
+notice.addEventListener("click", (e) => {
+  e.stopPropagation();
+  body.removeChild(notice);
 });
 
 const myLibrary = [];
@@ -48,7 +64,6 @@ function Book(title, author, pages, info, status) {
   this.author = author;
   this.pages = pages;
   this.info = info;
-  this.data = createBookData(title, author);
   this.status = status;
 }
 
@@ -63,37 +78,23 @@ function getStatus() {
   return value;
 }
 
-Book.prototype.UpdateStatus = function (status) {
-  switch (status.className) {
-    case "status unread":
-      status.className = "status reading";
-      status.textContent = "Reading";
-      break;
-    case "status reading":
-      status.className = "status finished";
-      status.textContent = "Finished";
-      break;
-    default:
-      status.className = "status unread";
-      status.textContent = "Not Read";
-      break;
-  }
-  this.status = status.textContent;
-};
-
-function createBookData(title, author) {
-  const data = [];
-  data.push(title.toLowerCase().split(" ")[0]);
-  data.push(author.toLowerCase().split(" ")[0]);
-  const bookData = data.reduce((datas, input) => {
-    if (input === data[1]) {
-      datas += input.split("")[0].toUpperCase();
-      datas += input.split("").splice(1, input.length).join("");
-    }
-    return datas;
-  });
-  return bookData;
-}
+// Book.prototype.UpdateStatus = function (status) {
+//   switch (status.className) {
+//     case "status unread":
+//       status.className = "status reading";
+//       status.textContent = "Reading";
+//       break;
+//     case "status reading":
+//       status.className = "status finished";
+//       status.textContent = "Finished";
+//       break;
+//     default:
+//       status.className = "status unread";
+//       status.textContent = "Not Read";
+//       break;
+//   }
+//   this.status = status.textContent;
+// };
 
 // function to get book details
 const bookTitle = document.querySelector("input#title");
@@ -106,55 +107,49 @@ submitBtn.addEventListener(
   "click",
   (e) => {
     e.stopPropagation();
-    checkSubmition(e);
+    checkSubmission(e);
   },
   false
 );
 
-function checkLibrary(newBook) {
-  const check1st = myLibrary.some((books) => books.title === this.title);
-  const check2nd = myLibrary.some((books) => books.author === this.author);
+async function checkLibrary(newBook) {
+  const data = await getDocs(collection(database, "books"));
+  const fetchedLibrary = [];
+  data.forEach((book) => {
+    const fetchedBook = book.data();
+    fetchedLibrary.push(fetchedBook);
+  });
+  const check1st = fetchedLibrary.some(
+    (books) => books.title === newBook.title
+  );
+  const check2nd = fetchedLibrary.some(
+    (books) => books.author === newBook.author
+  );
   if (check1st === true && check2nd === true) {
     body.appendChild(notice);
-    return false;
+    return true;
   }
-  return true;
-
+  return false;
 }
 
-function takeBookEntry() {
+async function takeBookEntry() {
   const newBook = new Book(
     bookTitle.value,
     bookAuthor.value,
     bookPages.value,
     bookInfo.value,
-    getStatus(),
+    getStatus()
   );
-
-  if (checkLibrary(newBook)) {
-    myLibrary.push(newBook);
+  const inLibrary = await checkLibrary(newBook);
+  if (!inLibrary) {
+    const bookRef = doc(collection(database, "books"));
+    console.log(bookRef);
+    console.log({ ...newBook, data: bookRef.id });
+    await setDoc(bookRef, { ...newBook, data: bookRef.id });
     return true;
   }
-  return false
+  return false;
 }
-
-function checkSubmition() {
-  event.preventDefault();
-  if (!bookTitle.checkValidity()) {
-    bookTitle.reportValidity();
-  } else if (!bookAuthor.checkValidity()) {
-    bookAuthor.reportValidity();
-  } else if (!bookPages.checkValidity()) {
-    bookPages.reportValidity();
-  } else if (takeBookEntry()) {
-    postBooks();
-    clearForm();
-    body.removeChild(form);
-  }
-}
-
-
-
 
 function clearForm() {
   bookTitle.value = "";
@@ -170,55 +165,39 @@ function clearForm() {
   });
 }
 
-function checkShelve() {
+function checkShelve(book) {
   const cards = shelve.querySelectorAll("div.card");
   let onShelve = false;
 
   cards.forEach((card) => {
     const data = card.getAttribute("data-book");
-    if (data === this.data) {
+    if (data === book.data) {
       onShelve = true;
     }
   });
   return onShelve;
 }
 
-function postBooks() {
-  myLibrary.forEach((books) => {
-    if (checkShelve.call(books) !== true) {
-      createCards.call(books);
-    }
+const updateStatus = async (status, bookData) => {
+  switch (status.className) {
+    case "status unread":
+      status.className = "status reading";
+      status.textContent = "Reading";
+      break;
+    case "status reading":
+      status.className = "status finished";
+      status.textContent = "Finished";
+      break;
+    default:
+      status.className = "status unread";
+      status.textContent = "Not Read";
+      break;
+  }
+  const bookRef = doc(database, "books", bookData);
+  await updateDoc(bookRef, {
+    status: status.textContent,
   });
-}
-
-function createCards() {
-  const title = document.createElement("h3");
-  title.setAttribute("class", "book-title");
-  title.textContent = this.title;
-
-  const author = document.createElement("p");
-  author.setAttribute("class", "author");
-  author.textContent = this.author;
-
-  const pages = document.createElement("p");
-  pages.setAttribute("class", "pages");
-  pages.textContent = `${this.pages} pages`;
-
-  const info = document.createElement("p");
-  info.setAttribute("class", "summary");
-  info.textContent = this.info;
-
-  const card = document.createElement("div");
-  card.setAttribute("class", "card");
-  card.setAttribute("data-book", this.data);
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages);
-  card.appendChild(info);
-  addStatus.call(card, this.status, this.data);
-  addModifyButton.call(card, this.data);
-  shelve.appendChild(card);
-}
+};
 
 function addStatus(value, data) {
   const status = document.createElement("p");
@@ -243,14 +222,25 @@ function addStatus(value, data) {
 
   status.addEventListener("click", (e) => {
     e.stopPropagation();
-    myLibrary.forEach((books) => {
-      if (books.data === data) {
-        books.UpdateStatus(status);
-      }
-    });
+    updateStatus(status, data);
   });
 
   this.appendChild(status);
+}
+
+const delBookFromDB = async (data) => {
+  await deleteDoc(doc(database, "books", `${data}`));
+};
+
+function deleteBook() {
+  const cards = shelve.querySelectorAll("div.card");
+  const data = this.getAttribute("data-book");
+  cards.forEach((card) => {
+    if (card.getAttribute("data-book") === data) {
+      shelve.removeChild(card);
+    }
+  });
+  delBookFromDB(data);
 }
 
 function addModifyButton(data) {
@@ -277,38 +267,68 @@ function addModifyButton(data) {
   this.appendChild(modify);
 }
 
-function deleteBook() {
-  const cards = shelve.querySelectorAll("div.card");
-  const data = this.getAttribute("data-book");
-  cards.forEach((card) => {
-    if (card.getAttribute("data-book") === data) {
-      shelve.removeChild(card);
-    }
-  });
-  const bookIndex = myLibrary.findIndex((books) => books.data === data);
-  myLibrary.splice(bookIndex, 1);
+function createCards(book) {
+  const title = document.createElement("h3");
+  title.setAttribute("class", "book-title");
+  title.textContent = book.title;
+
+  const author = document.createElement("p");
+  author.setAttribute("class", "author");
+  author.textContent = book.author;
+
+  const pages = document.createElement("p");
+  pages.setAttribute("class", "pages");
+  pages.textContent = `${book.pages} pages`;
+
+  const info = document.createElement("p");
+  info.setAttribute("class", "summary");
+  info.textContent = book.info;
+
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  card.setAttribute("data-book", book.data);
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(info);
+  addStatus.call(card, book.status, book.data);
+  addModifyButton.call(card, book.data);
+  shelve.appendChild(card);
 }
 
-const notice = document.querySelector("section#notice");
-notice.addEventListener("click", (e) => {
-  e.stopPropagation();
-  body.removeChild(notice);
-});
+const postBooks = async () => {
+  const cards = shelve.querySelectorAll("div.card");
+  if (cards.length) {
+    cards.forEach((card) => {
+      shelve.removeChild(card);
+    });
+  }
+  const library = await getDocs(collection(database, "books"));
+  library.forEach((book) => {
+    const fetchedBook = book.data();
+    createCards(fetchedBook);
+  });
+};
 
-const atomicHabits = new Book(
-  "Atomic Habits",
-  "James Clear",
-  "230",
-  "A revolutionary system to get 1 percent better every day. This Small changes will have a transformative effect on your career, your relationships and your life",
-  "Not Read",
-);
+async function checkSubmission(e) {
+  e.preventDefault();
+  if (!bookTitle.checkValidity()) {
+    bookTitle.reportValidity();
+  } else if (!bookAuthor.checkValidity()) {
+    bookAuthor.reportValidity();
+  } else if (!bookPages.checkValidity()) {
+    bookPages.reportValidity();
+  } else if (await takeBookEntry()) {
+    postBooks();
+    clearForm();
+    body.removeChild(form);
+  }
+}
 
 body.removeChild(form);
 body.removeChild(notice);
 
 form.classList.toggle("hidden");
 notice.classList.toggle("hidden");
-
-myLibrary.push(atomicHabits);
 
 postBooks();
